7a3bcd0ce6d446fb57ba74105c9b77d56a12a91a
Linda Horstmyer <linda@seashelldigital.com>
2015-11-01 19:51:31 -0500

    foobooks db

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     </title>
 
     <meta charset='utf-8'>
-    <link href="/css/foobooks.css" type='text/css' rel='stylesheet'>
+    <link href="/css/p3.css" type='text/css' rel='stylesheet'>
 
     {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
     @yield('head')
@@ -16,10 +16,10 @@
 <body>
 
     <header>
-        <img
-        src='http://making-the-internet.s3.amazonaws.com/laravel-foobooks-logo@2x.png'
-        style='width:300px'
-        alt='Foobooks Logo'>
+      <img
+     src='http://making-the-internet.s3.amazonaws.com/laravel-foobooks-logo@2x.png'
+     style='width:300px'
+     alt='Foobooks Logo'>
     </header>
 
     <section>
